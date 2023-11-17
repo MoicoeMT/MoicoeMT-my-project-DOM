24b8f3428af9e22f8987f7c59413a8960f517c1f
Moises Correa <145796144+MoicoeMT@users.noreply.github.com>
2023-11-17 02:13:16 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ["the", "our", "some", "another", "one"];
+let adj = ["great", "big", "Amazing", "funny", "cool"];
+let noun = ["jogger", "racoon", "website", "blog"];
+let dom = [".ve", ".de", ".com", ".org"];
+
+let domainGenerator = () => {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let o = 0; o < adj.length; o++) {
+      for (let e = 0; e < noun.length; e++) {
+        for (let u = 0; u < dom.length; u++) {
+          console.log(pronoun[i] + adj[o] + noun[e] + dom[u]);
+        }
+      }
+    }
+  }
 };
+
+domainGenerator();
+
+console.log("Hello World");
